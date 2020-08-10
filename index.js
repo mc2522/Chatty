036f0d19ccd8a0037d10cf41d7f658c7cacaa0f2
Mike Cao <mc2522@rit.edu>
2020-08-09 21:50:31 -0400

    sample html

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-
-require('dotenv').config()
-
-const PORT = process.env.PORT || 3000
-
-app.listen(PORT, () => console.log(`Listening on port ${PORT}...`))
