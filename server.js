ec1ca13c18038eb04340d9fae938935ef17fe0eb
Mike Cao <mc2522@rit.edu>
2020-08-10 23:05:24 -0400

    frontend

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
+const http = require('http')
 const path = require('path')
 const express = require('express')
+const socketio = require('socket.io')
+
 const app = express()
-//const server = require('http').server(app)
-//const io = require('socket.io')(server)
+const server = http.createServer(app)
+const io = socketio(server)
 
 require('dotenv').config()
 
 // static files
 app.use(express.static(path.join(__dirname, 'public')))
 
+io.on('connection', socket => {
+    console.log('New socket connection')
+    socket.emit('message', 'Welcome to Chat App')
+})
+
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}...`))
+server.listen(PORT, () => console.log(`Listening on port ${PORT}...`))
