879cf6eaaa42d6a5cff6bdfab4f91a4aba7a5eb2
Mike Cao <mc2522@rit.edu>
2020-08-13 14:17:02 -0400

    now stores all sockets and their associated names

--- server.js
+++ server.js
@@ -9,24 +9,60 @@ const io = socketio(server)
 
 require('dotenv').config()
 
-const users = new Set()
+// class that binds a name to a socket
+class Pair {
+    constructor(socket, name) {
+        this.socket = socket
+        this.name = name
+    }
+}
+
+// stores all in-use names
+const names = new Set()
+// stores all sockets
+const sockets = new Map()
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', socket => {
-    socket.emit('numberUsers', users.size)
+    sockets.set(socket, null)
+    // TEST console.log(sockets.size)
+    // send number of users data to newly connected socket
+    socket.emit('numberUsers', sockets.size)
+    // send message to all sockets
     socket.on('message', message => {
         io.emit('message', message)
     })
+    // check if name exists already, then add as appropriate
     socket.on('name', name => {
-        console.log(users.has(name))
-        if (users.has(name)) {
+        if (names.has(name)) {
+            // notify socket that username is unavailable
             socket.emit('user', false)
         } else {
-            users.add(name)
+            names.add(name)
+            // replace pairs
+            sockets.delete(socket)
+            sockets.set(socket, name)
+            // notify socket that username is available
             socket.emit('user', true)
-            io.emit('numberUsers', users.size)
+            // if users' size increases, send new number of users to all sockets
+            io.emit('numberUsers', sockets.size)
+            // TEST console.log(sockets.size)
+            // TEST console.log(names)
         }
+    })
+    // on disconnect, remove the socket and name
+    socket.on('disconnect', () => {
+        // remove name from in-use names
+        let name = sockets.get(socket)
+        if (name != null)
+            names.delete(name)
+        // remove socket pair
+        sockets.delete(socket)
+        // if users' size decreases, send new number of users to all sockets
+        io.emit('numberUsers', sockets.size)
+        // TEST console.log(sockets.size)
+        // TEST console.log(names)
     })
 })
 
