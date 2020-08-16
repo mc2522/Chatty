8f0708034862753b13956d5424b9feefd36bf79b
Mike Cao <mc2522@rit.edu>
2020-08-16 15:30:20 -0400

    redundant code

---
+++ public/script.js
@@ -0,0 +1,164 @@
+const socket = io()
+
+const name_input = document.getElementById('name-input')
+const submit_button = document.getElementById('submit-button')
+const identifier = document.getElementById('identifier')
+const name_message = document.getElementById('name-message')
+const online_counter = document.getElementById('online-counter')
+const message_box = document.getElementById('message-box') 
+const text = document.getElementById('text')
+const room_name = document.getElementById('room_name')
+const rooms = document.getElementsByClassName('room_button')
+const smalls = document.getElementsByTagName('small')
+const loading = document.getElementById('loading')
+const loader = document.getElementById('lds-ellipsis')
+
+let name = ''
+let selected_room = 'general'
+
+// When a message is received from server.js, display it in text 
+socket.on('message', message => {
+    let username = document.createElement('SMALL')
+    username.classList.add('text-name')
+    let content = document.createElement('SMALL')
+    content.classList.add('text-message')
+    let br = document.createElement('BR')
+    // class added for transition effect
+    username.classList.add('hide')
+    content.classList.add('hide')
+    // set text and styling
+    username.innerText = message.name
+    username.style.color = message.color
+    content.innerText = message.content
+    // add elements to text div
+    text.appendChild(username)
+    text.appendChild(content)
+    text.appendChild(br)
+    // transition to opacity: 1
+    setTimeout(() => {
+        username.classList.remove('hide')
+        content.classList.remove('hide')
+    }, 80)
+    // auto scroll to bottom
+    text.scrollTop = text.scrollHeight
+})
+
+// Receiving old messages from DB
+socket.on('history', messages => {
+
+})
+
+// Update number of users online TO BE IMPLEMENTED
+socket.on('numberUsers', number => {
+    online_counter.innerText = `${number} online`
+})
+
+// Check if user has been taken, assign if false
+socket.on('user', valid => {
+    trimName = name_input.value.trim()
+    if (valid) {
+        // reveal chat
+        identifier.style.opacity = "0";
+        // set name and disable interactivity with the identifier div
+        setTimeout(() => {
+            name = trimName
+            identifier.style.visibility = "hidden";
+        }, 500)
+    } else {
+        name = trimName
+        // message to user
+        name_message.innerText = 'Name has been taken already, select another name.'
+    }
+})
+
+// If reload, refresh the window because the user is somehow typing in a chat without id
+socket.on('reload', reload => {
+    if (reload) {
+        window.location.replace('/')
+    }
+})
+
+// Send name to server on enter
+name_input.addEventListener('keypress', e => {
+    if (e.keyCode == 13) {
+        e.preventDefault()
+        let username = name_input.value
+        // check if name is too long (max 20 characters)
+        if (username.length > 20) {
+            name_message.innerText = 'Name exceeds max characters (20).'
+        // check if only white space
+        } else if (!(/\S/gm).test(username)) {
+            name_message.innerText = 'Name needs to have at least one character.'
+        } else {
+            socket.emit('name', username.trim())
+        }
+    }
+})
+
+// send name to server on submit
+submit_button.addEventListener('click', e => {
+    e.preventDefault()
+    let username = name_input.value
+    // check if name is too long (max 20 characters)
+    if (username.length > 20) {
+        name_message.innerText = 'Name exceeds max characters (20).'
+    // check if only white space
+    } else if (!(/\S/gm).test(username)) {
+        name_message.innerText = 'Name needs to have at least one character.'
+    } else {
+        // send name to server for verification
+        socket.emit('name', username.trim())
+    }
+})
+
+// Press enter to send message and clear input 
+message_box.addEventListener('keypress', e => {
+    if (e.keyCode == 13) {  
+        e.preventDefault()
+        // check if only white space
+        if ((/\S/gm).test(message_box.value)) {
+            socket.emit('message', {
+                room_name: selected_room, 
+                content: message_box.value
+            })
+        }
+        message_box.value = ""
+    }
+})
+
+// Add event listener to room buttons
+Array.from(rooms).forEach(button => {
+    button.addEventListener('click', e => {
+        e.preventDefault()
+        socket.emit('change', button.id)
+        selected_room = button.id
+        // delete all children of text to delete messages since changing rooms
+        while (text.lastElementChild)
+            text.removeChild(text.lastElementChild)
+        // transition start for text
+        loading.style.visibility = 'visible'
+        loading.classList.add('show')
+        loader.childNodes.forEach(child => {
+            child.style.animationPlayState = 'running'
+        })
+        setTimeout(() => {
+            loading.classList.remove('show')
+        }, 500)
+        setTimeout(() => {
+            loading.style.visibility = 'hidden'
+            loader.childNodes.forEach(child => {
+                child.style.animationPlayState = 'paused'
+            })
+        }, 1000)
+        // transition end for text
+        // transition start for room name
+        room_name.classList.add('hide')
+        setTimeout(() => {
+            room_name.innerText = button.innerText
+        }, 250)
+        setTimeout(() => {
+            room_name.classList.remove('hide')
+        }, 250)
+        // transition end for room name
+    })
+})
---
+++ util.js
@@ -0,0 +1,112 @@
+const mongoose = require('mongoose')
+const please = require('pleasejs')
+
+require('dotenv').config()
+
+// Stores all rooms and their respective storages
+const rooms = new Map()
+
+// Create new mongoose model
+const createModel = room_name => {
+    const schema = new mongoose.Schema({
+        name: String,
+        message: String,
+        time: Number
+    }, { collection: room_name, size: 250 }) 
+    rooms.set(room_name, new mongoose.model(room_name, schema))
+}
+
+// Delete collection with name of room_name
+const deleteCollection = room_name => {
+    mongoose.connection.db.dropCollection(room_name, err => {
+        if (err) console.error(err)
+        console.log(`${room_name} was dropped.`)
+    })
+}
+
+// Clears all content in a collection or clear old messages depending on everything boolean
+const clearCollection = model => {
+    model.deleteMany({}, err => {
+        if (err) console.error(err)
+    })
+}
+
+// Gets all messages in the room
+const getMessages = room_name => {
+    return rooms.get(room_name).find({}, err => {
+        if (err) console.error(err)
+    })
+}
+
+// Save message in the correct collection
+const saveMessage = (room_name, name, message) => {
+    let selected_room = rooms.get(room_name)
+    if (selected_room != undefined) {
+        selected_room({
+            name: name,
+            message: message,
+            time: Date.now()
+        }).save(err => {
+            if (err) console.error(err)
+        })
+    } else {
+        // create room storage and save message
+        createModel(room_name)
+        rooms.get(room_name)({
+            name: name,
+            message: message,
+            time: Date.now()
+        }).save(err => {
+            if (err) console.error(err)
+        })
+    }
+}
+
+// Choose a random color for name display
+const randomColorPicker = () => {
+    return please.make_color({
+        saturation: 1,
+        value: 1,
+        golden: false,
+        format: 'hex'
+    })
+}
+
+// MongoDB setup
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@data-stno8.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+// Upon connecting, delete all unused collections
+mongoose.connection.on('open', () => {
+    console.log('Connected to Mongo.')
+    // get collection names
+    mongoose.connection.db.listCollections().toArray(async (err, names) => {
+        if (err) console.error(err)
+        for (let i = 0; i < names.length; i++) {
+            let collection_name = names[i].name
+            // check if collection should be deleted
+            if (collection_name != 'general') {
+                deleteCollection(collection_name)
+            } else {
+                clearCollection(rooms.get('general'))
+            }
+        }
+    })
+    // Every minute, delete old messages in DB for every model/room
+    setInterval(() => {
+        for (let model of rooms.values()) {
+            model.deleteMany({ time: { $lte: (Date.now() - 1200000) } }, err => {
+                if (err) console.error(err)
+            })
+        }
+    }, 60000)
+})
+
+module.exports = {
+    createModel,
+    deleteCollection,
+    randomColorPicker,
+    saveMessage
+}
